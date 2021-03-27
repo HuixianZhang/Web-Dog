a21b2901ce86af632b5e3da458c0d25533c601cb
HuixianZhang <32146234+HuixianZhang@users.noreply.github.com>
2021-03-27 19:26:22 -0400

    phrototype

---
+++ README.md
@@ -0,0 +1,5 @@
+The goal of this project is to create a online dog community for both existing and potential dog owners to learn about pictures of dogs.
+
+You can find more details below:
+
+https://docs.google.com/document/d/1bTJYk5uzU5H_K_M4XAJS84Dz-S17RkFz5bvX4BTeNQc/edit
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+const Home = () => {
+    return(
+        <div>
+            <h1>Home</h1>
+            <Link to="/search">
+                Search
+            </Link>
+            <br/>
+            <Link to="/details">
+                Detail
+            </Link>
+        </div>
+    )
+}
+
+export default Home
